6380f9feb2749211ebc9c1b4e6e35ba26f502729
theWorthyG <wdg2001@wildcats.unh.edu>
2019-03-26 18:18:44 -0400

    first upload

---
+++ bamazon_db.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE bamazon_db;
+
+USE bamazon_db;
+
+CREATE TABLE products (
+	-- Columns
+	ItemID INT NOT NULL AUTO_INCREMENT,
+	ProductName VARCHAR(200) NULL,
+	DepartmentName VARCHAR(200) NULL,
+	Price INT(6) DEFAULT 0,
+	StockQuantity INT (6) NOT NULL,
+  
+PRIMARY KEY (ItemID)
+);
---
+++ seeds.sql
@@ -0,0 +1,16 @@
+
+
+USE bamazon_db;
+
+-- 10 sample products
+INSERT INTO products (ProductName, departmentName, price, stockQuantity) values ("Xbox one", "Electronics", 299, 5);
+INSERT INTO products (ProductName, departmentName, price, stockQuantity) values ("PS4 Pro", "Electronics", 399, 5);
+INSERT INTO products (ProductName, departmentName, price, stockQuantity) values ("Xbox Elite Controller", "Electronics", 59, 5);
+INSERT INTO products (ProductName, departmentName, price, stockQuantity) values ("Oakley Sunglasses", "Clothes", 199, 5);
+INSERT INTO products (ProductName, departmentName, price, stockQuantity) values ("Gucci shirt ", "Clothes", 300, 5);
+INSERT INTO products (ProductName, departmentName, price, stockQuantity) values ("Vans Shoes", "Clothes", 59, 5);
+INSERT INTO products (ProductName, departmentName, price, stockQuantity) values ("Bud Light 30pk", "Food", 19, 5);
+INSERT INTO products (ProductName, departmentName, price, stockQuantity) values ("Paintball gun", "H1obby", 400, 5);
+INSERT INTO products (ProductName, departmentName, price, stockQuantity) values ("Dirtbike", "Vehicles", 3000, 5);
+INSERT INTO products (ProductName, departmentName, price, stockQuantity) values ("Flamethrower", "Tools", 5, 5);
+
